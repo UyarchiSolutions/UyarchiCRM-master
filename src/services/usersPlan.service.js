118bdbfde6ee4b7399c834858caf14ab6a329226
UyarchiSolutions <gitadmin@uyarchi.com>
2023-01-26 17:09:14 +0530

    asdf

--- src/services/usersPlan.service.js
+++ src/services/usersPlan.service.js
@@ -14,8 +14,13 @@ const createUserPlan = async (body, id) => {
 
   if (PlanRole === 'Buyer') {
     let planvalid = moment().add(plan.PlanValidate, 'days');
-    let existPlan = usersPlan.findOne({}).sort({ created: -1 });
-    console.log(existPlan);
+    let current = moment().toDate();
+    let existPlan = await usersPlan
+      .findOne({ userId: id, PlanId: PlanId, ContactNumber: { $gt: 0 }, planValidate: { $gt: current } })
+      .sort({ created: -1 });
+    if (existPlan) {
+      throw new ApiError(httpStatus.BAD_REQUEST, 'plan Limit Not Exceeded or Plan Validity Not Expired');
+    }
     values = { ...body, ...{ created: moment(), userId: id, planValidate: planvalid } };
   }
   if (PlanRole === 'Seller') {
