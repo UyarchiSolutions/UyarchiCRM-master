e18cedc24fce05ab84ec029c4c37404139fa7190
muthamizhyadav <muthamizhyadav@gmail.com>
2023-03-22 14:54:26 +0530

    asdf

--- src/services/BuyerSeller.service.js
+++ src/services/BuyerSeller.service.js
@@ -365,7 +365,7 @@ const getApprover_Property = async (query, userId) => {
           typeMatch,
           formatAdd,
           // { propStatus: 'Approved' },
-          { finsh: true },
+          { finsh: query.finish },
         ],
       },
     },
