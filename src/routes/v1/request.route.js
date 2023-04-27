942d15d680643ae480072de571ec8e2a06557f1b
muthamizhselvan R <muthamizhyadav@gmail.com>
2023-04-27 17:40:40 +0530

    asdf

--- src/routes/v1/request.route.js
+++ src/routes/v1/request.route.js
@@ -8,5 +8,5 @@ router.route('/:id').get(RequestStreamController.getRequsetStreamById);
 router.route('/:id').put(RequestStreamController.UpdateRequestStream);
 router.route('/get/Streams').get(Authorization, RequestStreamController.getStreams);
 router.route('/getStreams/Admin/Side').get(RequestStreamController.getStreams_Admin_Side);
-router.route('/AdminStream/Approved/Cancel/:id').get(RequestStreamController.AdminStream_Approved_Cancel);
+router.route('/AdminStream/Approved/Cancel/:id').put(RequestStreamController.AdminStream_Approved_Cancel);
 module.exports = router;
