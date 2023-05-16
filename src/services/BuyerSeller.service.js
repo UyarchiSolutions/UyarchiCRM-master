3f072612bd05972a0dfd4943ecfa864490980aa2
muthamizhselvan R <muthamizhyadav@gmail.com>
2023-05-16 15:19:54 +0530

    asd

--- src/services/BuyerSeller.service.js
+++ src/services/BuyerSeller.service.js
@@ -2373,7 +2373,7 @@ const PropertyDeatails_after_intrested = async (id) => {
     {
       $match: {
         propertyId: id,
-        status: { $in: ['Intrested', 'Rejected', 'Shcedule', 'Ignored', 'Accept', 'Visited', 'Fixed', 'Re-schedule'] },
+        status: { $in: ['Intrested', 'Rejected', 'Shcedule', 'Ignored', 'Accept', 'Visited', 'Fixed', 'Reschedule'] },
       },
     },
     {
