99762e90604cee621bb35e2db43fa4b3c11833b2
muthamizhyadav <muthamizhyadav@gmail.com>
2023-06-13 10:08:49 +0530

    asdf

--- src/models/jobEnquiry.model.js
+++ src/models/jobEnquiry.model.js
@@ -1,17 +1,16 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
-const { v4}=require('uuid')
+const { v4 } = require('uuid');
 const { toJSON, paginate } = require('./plugins');
 
 const jobEnquirySchema = mongoose.Schema({
-  _id :{
-    type : String,
-    default:v4
+  _id: {
+    type: String,
+    default: v4,
   },
   name: {
     type: String,
-    
   },
   email: {
     type: String,
@@ -22,6 +21,7 @@ const jobEnquirySchema = mongoose.Schema({
   Enquiry: {
     type: String,
   },
+  EnquiryId: String,
 });
 jobEnquirySchema.plugin(toJSON);
 jobEnquirySchema.plugin(paginate);
--- src/services/jobEnquiry.service.js
+++ src/services/jobEnquiry.service.js
@@ -4,12 +4,29 @@ const ApiError = require('../utils/ApiError');
 const jobEnquirymodel = require('../models/jobEnquiry.model');
 
 const createjobEnquiry = async (body) => {
-  const jobEnquiry = await jobEnquirymodel.create(body);
+  let findOrders = await jobEnquirymodel.find().count();
+  let center = '';
+  if (findOrders < 9) {
+    center = '0000';
+  }
+  if (findOrders < 99 && findOrders >= 9) {
+    center = '000';
+  }
+  if (findOrders < 999 && findOrders >= 99) {
+    center = '00';
+  }
+  if (findOrders < 9999 && findOrders >= 999) {
+    center = '0';
+  }
+  let count = findOrders + 1;
+  let orderId = `ENQ${center}${count}`;
+
+  const jobEnquiry = await jobEnquirymodel.create(...body, { EnquiryId: orderId });
   return jobEnquiry;
 };
 
 const getjobEnquiry = async () => {
-  const jobEnquiry = await jobEnquirymodel.find();
+  const jobEnquiry = await jobEnquirymodel.find({ active: true });
   return jobEnquiry;
 };
 
